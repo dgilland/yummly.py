045209be07bd28a7120d11797bdf48c2e410d38f
Derrick Gilland <dgilland@gmail.com>
2014-11-22 09:27:54 -0500

    Release v0.4.1.

--- yummly/__init__.py
+++ yummly/__init__.py
@@ -1,7 +1,7 @@
 
 from client import Client, YummlyError, Timeout
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __author__ = 'Derrick Gilland <dgilland@gmail.com>'
 __all__ = ['Client', 'YummlyError', 'Timeout']
 
