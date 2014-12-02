eb12092fbcd5f05f33bdc3eb4a1b59351f7ac815
Derrick Gilland <dgilland@gmail.com>
2014-12-01 22:22:32 -0500

    Release v0.5.0.

--- yummly/__meta__.py
+++ yummly/__meta__.py
@@ -5,7 +5,7 @@ __title__ = 'yummly'
 __summary__ = 'Python library for Yummly API: https://developer.yummly.com'
 __url__ = 'https://github.com/dgilland/yummly.py'
 
-__version__ = '0.4.1'
+__version__ = '0.5.0'
 __install_requires__ = ['requests>=1.1.0']
 
 __author__ = 'Derrick Gilland'
