f0e19e8fac5602ea7a7bb21f0c1b30916a68f7a1
Derrick Gilland <dgilland@gmail.com>
2014-12-01 22:14:30 -0500

    Refactor setup.py.
    - Add __meta__.py file for project data.
    - Use tox, twine, and wheel.
    - Add test requirements to main requirements file.
    - Expand makefile targets with various build, test, and distribution
    targets.

--- setup.py
+++ setup.py
@@ -1,30 +1,70 @@
+"""
+yummly.py
+=========
 
-from setuptools import setup
+Project: https://github.com/dgilland/yummly.py
 
-with open('VERSION', 'r') as f:
-    version = f.read()
+Documentation: https://github.com/dgilland/yummly.py
+"""
 
-with open('README', 'r') as f:
-    long_description = f.read()
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+meta = {}
+exec(read('yummly/__meta__.py'), meta)
+
+
+class Tox(TestCommand):
+    user_options = [
+        ('tox-args=', 'a', "Arguments to pass to tox")
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = '-c tox.ini'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Import here because outside the eggs aren't loaded.
+        import tox
+        import shlex
+
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
 
 setup(
-    name                = 'yummly',
-    version             = version,
-    description         = 'Python package for Yummly API: https://developer.yummly.com',
-    long_description    = long_description,
-    author              = 'Derrick Gilland',
-    author_email        = 'dgilland@gmail.com',
-    url                 = 'https://github.com/dgilland/yummly.py',
-    packages            = [ 'yummly' ],
-    install_requires    = ['requests>=1.1.0'],
-    keywords            = 'yummly recipes',
-    license             = 'BSD',
-    classifiers         = [
+setup(
+    name=meta['__title__'],
+    version=meta['__version__'],
+    url=meta['__url__'],
+    license=meta['__license__'],
+    author=meta['__author__'],
+    author_email=meta['__email__'],
+    description=meta['__summary__'],
+    long_description=read('README.rst'),
+    packages=find_packages(exclude=['tests']),
+    install_requires=meta['__install_requires__'],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+    test_suite='tests',
+    classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'License :: OSI Approved :: BSD License'
+        'License :: OSI Approved :: MIT License'
     ]
 )
 
---
+++ yummly/__meta__.py
@@ -0,0 +1,14 @@
+"""Project metadata.
+"""
+
+__title__ = 'yummly'
+__summary__ = 'Python library for Yummly API: https://developer.yummly.com'
+__url__ = 'https://github.com/dgilland/yummly.py'
+
+__version__ = '0.4.1'
+__install_requires__ = ['requests>=1.1.0']
+
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+
+__license__ = 'MIT License'
